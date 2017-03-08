7b2bfbc7d695488d33afa5e640b76a77c0047d0f
sjonchhe <sjonchhe@outlook.com>
2017-03-08 22:10:51 +0545

    New page!Result Page aded
    New page ie result page has been added.It serves as a result viewing
    place  where students can enter their username or examination symbol
    number and see their marksheet ie result.This only contains the front
    end part!

---
+++ result.php
@@ -0,0 +1,20 @@
+
+<html>
+<head><title>Result | St.Lawrence College</title></head>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<link rel="stylesheet" type="text/css" href="style1.css">
+<link rel="shortcut icon" href="gallery/stlawrence.PNG"/>
+
+<body>
+
+<div class="result_wrapper">
+  <a href="javascript:history.back()"><img src="gallery/logo/home1.png" style="height:50px;width:50px;position:absolute;top:50px;left:20px;"></a>
+  <div class="resultsearch grid60">
+    <form method="POST">
+      <input type="text" placeholder="Enter your Username or your Symbol number" name="rsymnum" class="grid50">
+      <input type="submit" value="Search" class="button grid20 back-trans text-white">
+    </form>
+
+  </div>
+</div>
