a2de4f9248aed38a0da2602ba69e0b6642681cca
sjonchhe <sjonchhe@outlook.com>
2017-03-11 18:49:21 +0545

    NEW PAge!Marksheet!
    Marksheet page has been added

---
+++ marksheet.php
@@ -0,0 +1,77 @@
+<html>
+<head><title>Marksheet | Result | St.Lawrence College</title></head>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<link rel="stylesheet" type="text/css" href="style1.css">
+<link rel="shortcut icon" href="gallery/stlawrence.PNG"/>
+
+<body>
+  <!-- COLLEGE INFORMATION-->
+<div class="marksheet_wrapper">
+        <div id="collegein"><img src="gallery/stlawrence.png">
+      <center><span style="font-size:40px;font-weight:bolder;">ST.LAWRENCE COLLEGE</span><BR>
+              <span style="font-size:21px;font-weight:bolder;">CHABAHIL,KATHMANDU<br></span>
+              <span style="font-size:17px;font-weight:bolder;">4515313<br></span><br><br>
+              <span style="font-size:37px;font-weight:400;background:silver;padding:5px;border-radius:5px;margin:5px;">MARKSHEET<br></span>
+              <span style="font-size:25px;font-weight:400;text-transform:uppercase;">Bachelor of BSC CSIT</span><br>
+              <span style="font-size:23px;font-weight:400;">Third year Fifth Semester</span><br>
+
+
+      </div>
+
+  <!--STUDENT DETAIL-->
+      <center>
+      <table class="msdetail bold" >
+        <tr>
+          <td>Name: Ram Prasad </td>
+          <td>Roll No:  33</td>
+        </tr>
+        <tr><td colspan="2">Semester:  Fifth</td></tr>
+          <tr><td colspan="2">Batch: 2015</td></tr>
+
+      </table>
+      <!--TABLE FOR MARKSHEET-->
+      <table class="marksheet_table">
+        <tr style="background:silver;height:60px;font-weight:bolder;text-transform:uppercase;">
+          <td>subject code</td><td>Subject</td><td>Full marks</td><td>Pass Marks</td><td>Marks Obtained</td><td>Highest Mark</td>
+        </tr>
+        <tr>
+          <td>CSC-503</td><td>Introduction to Artificial Intelligence</td><td>60</td><td>24</td><td>48</td><td>52</td>
+        </tr>
+        <tr>
+          <td>CSC-503</td><td>Design analysis and algorithm</td><td>60</td><td>24</td><td>42</td><td>56</td>
+        </tr>
+        <tr>
+          <td>CSC-503</td><td>Simulation</td><td>60</td><td>24</td><td>40</td><td>46</td>
+        </tr>
+        <tr>
+          <td>CSC-503</td><td>E-Governance</td><td>60</td><td>24<td>49</td><td>55</td>
+        </tr>
+        <tr>
+          <td>CSC-503</td><td>Computer Network</td><td>60</td><td>24<td>49</td><td>55</td>
+        </tr>
+        <tr><td></td><td></td><Td></td><td></td><td></td><td></td></tr>
+
+          <tr><td></td><td></td><td></td><Td></td><td></td><td></td></tr>
+          <tr><td></td><td></td><Td></td><td></td><td></td><td></td></tr>
+          <tr><td></td><td></td><Td></td><td></td><td></td><td></td></tr>
+          <tr><td></td><td></td><Td></td><td></td><td></td><td></td></tr>
+          <tr><td></td><td></td><Td></td><td></td><td></td><td></td></tr>
+          <tr><td></td><td></td><Td></td><td></td><td></td><td></td></tr>
+            <tr >
+              <td></td><td></td><td></td><td colspan="3" style="text-align:right;font-weight:bolder;">Grand Total : 300</td>
+            </tr>
+            <tr style="text-align:right;font-weight:bolder;">
+              <td colspan="3"></td><td colspan="2" >Percentage : 65%</td><td>RANK : 15th</td></tr>
+              <tr style="text-align:right;font-size:12px;"><td>ATTENDANCE  :  72/78</td><td colspan="5">REMARKS:<br> ABS  : ABSENT<BR> * : FAILED</td></tr>
+      </table>
+
+    <div class="after_table bold">
+        DATE OF ISSUE :  8/20/2017<br>
+        FACULTY INCHARGE: Ram Prasad
+    </div>
+
+
+</div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Result | St.Lawrence College</title></head>
+<link rel="stylesheet" type="text/css" href="test.css">
+<body>
+
+<p class="test ">
+Test subject
+
+</p>
+</body>
+</html>
